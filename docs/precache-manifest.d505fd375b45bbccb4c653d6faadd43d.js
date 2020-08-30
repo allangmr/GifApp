279eb53c958a43f8bf50c54d560d5229417158fd
Allan Montilla <allangmr7@gmail.com>
2020-08-29 19:49:42 -0500

    docs agregado

---
+++ docs/precache-manifest.d505fd375b45bbccb4c653d6faadd43d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644c926804dde3220673df52ae982664",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c4eb46fc094066c879c",
+    "url": "/static/css/main.14c159f6.chunk.css"
+  },
+  {
+    "revision": "6b1578778fd38a87f0cb",
+    "url": "/static/js/2.a685c4d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a685c4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c4eb46fc094066c879c",
+    "url": "/static/js/main.9733cc6b.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
